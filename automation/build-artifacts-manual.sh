254a84d857fb6d4424c8aaf15210a7de070016f0
Francesco Romani <fromani@redhat.com>
2018-04-24 10:22:33 +0200

    packaging: prepare for release
    the build infrastructure needed some updates to catch up,
    this patch implements them.
    
    Change-Id: Iad62decd3ae2d5775516fc8b10084d6a3c986123
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- automation/build-artifacts-manual.sh
+++ automation/build-artifacts-manual.sh
@@ -5,15 +5,12 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
-
 autoreconf -ivf
 ./configure
 yum-builddep ovirt-vmconsole.spec
 # Run rpmbuild, assuming the tarball is in the project's directory
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
-    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-vmconsole*.tar.gz
 
 mv *.tar.gz exported-artifacts
